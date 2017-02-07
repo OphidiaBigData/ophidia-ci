3285a20277c11c1e44d80c808f3973ccde0c12e1
Cosimo Palazzo <cosimo.palazzo@cmcc.it>
2017-02-07 18:28:54 +0100

    Include code checking for all the packages

--- codestyle/main.sh
+++ codestyle/main.sh
@@ -18,34 +18,169 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-if [ $# -ne 2 ]
+if [ $# -lt 2 ]
 then
         echo "The following arguments are required: buildtype (master, devel, etc.), workspace (where there are the sources)"
+        echo "The following arguments are optional: package (terminal, primitives, io-server, server or analytics-framework)"
         exit 1
 fi
 
 buildtype=$1
 WORKSPACE=$2
+package=$3
+
+function check_folder {
+	find $1 -name $2 -type f -print0 | xargs -0 indent -kr -cli8 -i8 -l200
+}
 
 # codestyle check for Ophidia Server
 
 cd $WORKSPACE
 git checkout ${buildtype}
 
-find src/ -name 'debug.h' -type f -print0 | xargs -0 indent -kr -cli8 -i8 -l200
-find src/ -name 'debug.c' -type f -print0 | xargs -0 indent -kr -cli8 -i8 -l200
-find src/ -name 'oph_*.h' -type f -print0 | xargs -0 indent -kr -cli8 -i8 -l200
-find src/ -name 'oph_*.c' -type f -print0 | xargs -0 indent -kr -cli8 -i8 -l200
-find test/ -name 'oph_*.c' -type f -print0 | xargs -0 indent -kr -cli8 -i8 -l200
+if [ "${package}" == "primitives" ] || [ $# -lt 3 ]; then
 
-R=$(git status | grep "modified" | wc -l)
-if [ $R -eq 0 ]
-	then 
-		echo "SUCCESS: all files are compliant with coding style"
-		$(exit 0) 
-	else 
-		echo "WARNING: found $R files not compliant:"
-		git status | grep "modified" | sed -n "s/^.*modified:[ ]*//p"
-		$(exit 1) 
+	check_folder src/ 'oph_*.h'
+	check_folder src/ 'oph_*.c'
+
+	R=$(git status | grep "modified" | wc -l)
+	if [ $R -eq 0 ]
+		then 
+			echo "SUCCESS: all files are compliant with coding style"
+			$(exit 0) 
+		else 
+			echo "WARNING: found $R files not compliant:"
+			git status | grep "modified" | sed -n "s/^.*modified:[ ]*//p"
+			$(exit 1) 
+	fi
+fi
+
+if [ "${package}" == "io-server" ] || [ $# -lt 3 ]; then
+
+	check_folder src/client/ 'oph_*.h'
+	check_folder src/client/ 'oph_*.c'
+	check_folder src/common/ 'debug.h'
+	check_folder src/common/ 'debug.c'
+	check_folder src/common/ 'oph_*.h'
+	check_folder src/common/ 'oph_*.c'
+	check_folder src/devices/ '*.h'
+	check_folder src/devices/ '*.c'
+	check_folder src/iostorage/ 'oph_*.h'
+	check_folder src/iostorage/ 'oph_*.c'
+	check_folder src/metadb/ 'oph_*.h'
+	check_folder src/metadb/ 'oph_*.c'
+	check_folder src/network/ 'oph_*.h'
+	check_folder src/network/ 'oph_*.c'
+	check_folder src/query_engine/ 'oph_query_engine*.h'
+	check_folder src/query_engine/ 'oph_query_engine*.c'
+	check_folder src/query_engine/ 'oph_query_expression_client.c'
+	check_folder src/query_engine/ 'oph_query_expression_evaluator.h'
+	check_folder src/query_engine/ 'oph_query_expression_evaluator.c'
+	check_folder src/query_engine/ 'oph_query_expression_functions.h'
+	check_folder src/query_engine/ 'oph_query_expression_functions.c'
+	check_folder src/query_engine/ 'oph_query_plugin*.h'
+	check_folder src/query_engine/ 'oph_query_plugin*.c'
+	check_folder src/query_engine/ 'oph_query_parser.h'
+	check_folder src/query_engine/ 'oph_query_parser.c'
+	check_folder src/server/ 'oph_*.h'
+	check_folder src/server/ 'oph_*.c'
+
+	R=$(git status | grep "modified" | wc -l)
+	if [ $R -eq 0 ]
+		then 
+			echo "SUCCESS: all files are compliant with coding style"
+			$(exit 0) 
+		else 
+			echo "WARNING: found $R files not compliant:"
+			git status | grep "modified" | sed -n "s/^.*modified:[ ]*//p"
+			$(exit 1) 
+	fi
+fi
+
+if [ "${package}" == "analytics-framework" ] || [ $# -lt 3 ]; then
+
+	check_folder include/ 'debug.h'
+	check_folder include/ 'oph_*.h'
+	check_folder include/drivers/ '*.h'
+	check_folder include/ophidiadb/ '*.h'
+	check_folder include/ophidiadb/query/ '*.h'
+	check_folder include/oph_ioserver/ '*.h'
+	check_folder include/oph_json/ '*.c'
+	check_folder include/query/ '*.h'
+	check_folder src/ 'debug.c'
+	check_folder src/ 'oph_*.c'
+	check_folder src/clients/ '*.c'
+	check_folder src/clients/ '*.h'
+	check_folder src/drivers/ '*.c'
+	check_folder src/ioservers/ '*.c'
+	check_folder src/ioservers/ '*.h'
+	check_folder src/oph_gsoap/ 'oph_*.h'
+	check_folder src/oph_gsoap/ssl 'oph_*.c'
+	check_folder src/oph_gsoap/gsi 'oph_*.c'
+	check_folder src/ophidiadb/ '*.c'
+	check_folder src/oph_ioserver/ '*.c'
+	check_folder src/oph_ioserver/ '*.h'
+	check_folder src/oph_json/ '*.c'
+
+	R=$(git status | grep "modified" | wc -l)
+	if [ $R -eq 0 ]
+		then 
+			echo "SUCCESS: all files are compliant with coding style"
+			$(exit 0) 
+		else 
+			echo "WARNING: found $R files not compliant:"
+			git status | grep "modified" | sed -n "s/^.*modified:[ ]*//p"
+			$(exit 1) 
+	fi
+fi
+
+if [ "${package}" == "server" ] || [ $# -lt 3 ]; then
+
+	check_folder src/ 'debug.h'
+	check_folder src/ 'debug.c'
+	check_folder src/ 'oph_*.h'
+	check_folder src/ 'oph_*.c'
+	check_folder test/ 'oph_*.c'
+
+	R=$(git status | grep "modified" | wc -l)
+	if [ $R -eq 0 ]
+		then 
+			echo "SUCCESS: all files are compliant with coding style"
+			$(exit 0) 
+		else 
+			echo "WARNING: found $R files not compliant:"
+			git status | grep "modified" | sed -n "s/^.*modified:[ ]*//p"
+			$(exit 1) 
+	fi
+fi
+
+if [ "${package}" == "terminal" ] || [ $# -lt 3 ]; then
+
+	check_folder src/ 'oph_*.h'
+	check_folder src/ 'oph_*.c'
+	check_folder src/client/ 'oph_*.h'
+	check_folder src/client/ssl/ 'oph_*.c'
+	check_folder src/client/gsi/ 'oph_*.c'
+	check_folder src/env/ 'oph_*.h'
+	check_folder src/env/ 'oph_*.c'
+	check_folder src/help/ 'oph_*.h'
+	check_folder src/help/ 'oph_*.c'
+	check_folder src/viewer/ 'oph_*.h'
+	check_folder src/viewer/ 'oph_*.c'
+	check_folder src/viewer/oph_json/ 'oph_*.h'
+	check_folder src/viewer/oph_json/ 'oph_*.c'
+	check_folder src/workflow/ 'oph_*.h'
+	check_folder src/workflow/ 'oph_*.c'
+
+	R=$(git status | grep "modified" | wc -l)
+	if [ $R -eq 0 ]
+		then 
+			echo "SUCCESS: all files are compliant with coding style"
+			$(exit 0) 
+		else 
+			echo "WARNING: found $R files not compliant:"
+			git status | grep "modified" | sed -n "s/^.*modified:[ ]*//p"
+			$(exit 1) 
+	fi
 fi
 
