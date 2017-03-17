4aed010b2eb9382738fc716bfe360d4711c32526
eldoo <eldoo@users.noreply.github.com>
2017-03-17 17:20:29 +0100

    Add code style check for PyOphidia

--- codestyle/main.sh
+++ codestyle/main.sh
@@ -18,10 +18,12 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+set -e
+
 if [ $# -lt 2 ]
 then
         echo "The following arguments are required: buildtype (master, devel, etc.), workspace (where there are the sources)"
-        echo "The following arguments are optional: package (terminal, primitives, io-server, server or analytics-framework)"
+        echo "The following arguments are optional: package (terminal, primitives, io-server, server, analytics-framework or PyOphidia)"
         exit 1
 fi
 
@@ -29,9 +31,15 @@ buildtype=$1
 WORKSPACE=$2
 package=$3
 
-function check_folder {
-	find $1 -name $2 -type f -print0 | xargs -0 indent -kr -cli8 -i8 -l200
+function check_folder  {
+	if [ "${1}" == "c" ]; then
+		find $2 -name $3 -type f -print0 | xargs -0 indent -kr -cli8 -i8 -l200
+	else
+		eval find $2 -name $3 -type f -print0 | xargs -0 autopep8 --max-line-length 200 --in-place --ignore E128
+	fi
 }
+
+
 
 # codestyle check for Ophidia Server
 
@@ -40,8 +48,8 @@ git checkout ${buildtype}
 
 if [ "${package}" == "primitives" ] || [ $# -lt 3 ]; then
 
-	check_folder src/ 'oph_*.h'
-	check_folder src/ 'oph_*.c'
+	check_folder "c" src/ 'oph_*.h'
+	check_folder "c" src/ 'oph_*.c'
 
 	R=$(git status | grep "modified" | wc -l)
 	if [ $R -eq 0 ]
@@ -57,33 +65,33 @@ fi
 
 if [ "${package}" == "io-server" ] || [ $# -lt 3 ]; then
 
-	check_folder src/client/ 'oph_*.h'
-	check_folder src/client/ 'oph_*.c'
-	check_folder src/common/ 'debug.h'
-	check_folder src/common/ 'debug.c'
-	check_folder src/common/ 'oph_*.h'
-	check_folder src/common/ 'oph_*.c'
-	check_folder src/devices/ '*.h'
-	check_folder src/devices/ '*.c'
-	check_folder src/iostorage/ 'oph_*.h'
-	check_folder src/iostorage/ 'oph_*.c'
-	check_folder src/metadb/ 'oph_*.h'
-	check_folder src/metadb/ 'oph_*.c'
-	check_folder src/network/ 'oph_*.h'
-	check_folder src/network/ 'oph_*.c'
-	check_folder src/query_engine/ 'oph_query_engine*.h'
-	check_folder src/query_engine/ 'oph_query_engine*.c'
-	check_folder src/query_engine/ 'oph_query_expression_client.c'
-	check_folder src/query_engine/ 'oph_query_expression_evaluator.h'
-	check_folder src/query_engine/ 'oph_query_expression_evaluator.c'
-	check_folder src/query_engine/ 'oph_query_expression_functions.h'
-	check_folder src/query_engine/ 'oph_query_expression_functions.c'
-	check_folder src/query_engine/ 'oph_query_plugin*.h'
-	check_folder src/query_engine/ 'oph_query_plugin*.c'
-	check_folder src/query_engine/ 'oph_query_parser.h'
-	check_folder src/query_engine/ 'oph_query_parser.c'
-	check_folder src/server/ 'oph_*.h'
-	check_folder src/server/ 'oph_*.c'
+	check_folder "c" src/client/ 'oph_*.h'
+	check_folder "c" src/client/ 'oph_*.c'
+	check_folder "c" src/common/ 'debug.h'
+	check_folder "c" src/common/ 'debug.c'
+	check_folder "c" src/common/ 'oph_*.h'
+	check_folder "c" src/common/ 'oph_*.c'
+	check_folder "c" src/devices/ "*.h"
+	check_folder "c" src/devices/ "*.c"
+	check_folder "c" src/iostorage/ 'oph_*.h'
+	check_folder "c" src/iostorage/ 'oph_*.c'
+	check_folder "c" src/metadb/ 'oph_*.h'
+	check_folder "c" src/metadb/ 'oph_*.c'
+	check_folder "c" src/network/ 'oph_*.h'
+	check_folder "c" src/network/ 'oph_*.c'
+	check_folder "c" src/query_engine/ 'oph_query_engine*.h'
+	check_folder "c" src/query_engine/ 'oph_query_engine*.c'
+	check_folder "c" src/query_engine/ 'oph_query_expression_client.c'
+	check_folder "c" src/query_engine/ 'oph_query_expression_evaluator.h'
+	check_folder "c" src/query_engine/ 'oph_query_expression_evaluator.c'
+	check_folder "c" src/query_engine/ 'oph_query_expression_functions.h'
+	check_folder "c" src/query_engine/ 'oph_query_expression_functions.c'
+	check_folder "c" src/query_engine/ 'oph_query_plugin*.h'
+	check_folder "c" src/query_engine/ 'oph_query_plugin*.c'
+	check_folder "c" src/query_engine/ 'oph_query_parser.h'
+	check_folder "c" src/query_engine/ 'oph_query_parser.c'
+	check_folder "c" src/server/ 'oph_*.h'
+	check_folder "c" src/server/ 'oph_*.c'
 
 	R=$(git status | grep "modified" | wc -l)
 	if [ $R -eq 0 ]
@@ -99,25 +107,26 @@ fi
 
 if [ "${package}" == "analytics-framework" ] || [ $# -lt 3 ]; then
 
-	check_folder include/ 'debug.h'
-	check_folder include/ 'oph_*.h'
-	check_folder include/drivers/ '*.h'
-	check_folder include/ophidiadb/ '*.h'
-	check_folder include/oph_ioserver/ '*.h'
-	check_folder include/oph_json/ '*.c'
-	check_folder include/query/ '*.h'
-	check_folder src/ 'debug.c'
-	check_folder src/ 'oph_*.c'
-	check_folder src/clients/ '*.c'
-	check_folder src/clients/ '*.h'
-	check_folder src/drivers/ '*.c'
-	check_folder src/ioservers/ '*.c'
-	check_folder src/ioservers/ '*.h'
-	check_folder src/oph_gsoap/ 'oph_*.h'
-	check_folder src/ophidiadb/ '*.c'
-	check_folder src/oph_ioserver/ '*.c'
-	check_folder src/oph_ioserver/ '*.h'
-	check_folder src/oph_json/ '*.c'
+	check_folder "c" include/ 'debug.h'
+	check_folder "c" include/ 'oph_*.h'
+	check_folder "c" include/drivers/ "*.h"
+	check_folder "c" include/ophidiadb/ "*.h"
+	check_folder "c" include/oph_ioserver/ "*.h"
+	check_folder "c" include/oph_json/ "*.h"
+	check_folder "c" include/query/ "*.h"
+	check_folder "c" src/ 'debug.c'
+	check_folder "c" src/ 'oph_*.c'
+	check_folder "c" src/clients/ "*.c"
+	check_folder "c" src/clients/ "*.h"
+	check_folder "c" src/drivers/ "*.c"
+	check_folder "c" src/ioservers/ "*.c"
+	check_folder "c" src/ioservers/ "*.h"
+	check_folder "c" src/oph_gsoap/ 'oph_*.h'
+	check_folder "c" src/oph_gsoap/ 'oph_*.c'
+	check_folder "c" src/ophidiadb/ "*.c"
+	check_folder "c" src/oph_ioserver/ "*.c"
+	check_folder "c" src/oph_ioserver/ "*.h"
+	check_folder "c" src/oph_json/ "*.c"
 
 	R=$(git status | grep "modified" | wc -l)
 	if [ $R -eq 0 ]
@@ -133,11 +142,11 @@ fi
 
 if [ "${package}" == "server" ] || [ $# -lt 3 ]; then
 
-	check_folder src/ 'debug.h'
-	check_folder src/ 'debug.c'
-	check_folder src/ 'oph_*.h'
-	check_folder src/ 'oph_*.c'
-	check_folder test/ 'oph_*.c'
+	check_folder "c" src/ 'debug.h'
+	check_folder "c" src/ 'debug.c'
+	check_folder "c" src/ 'oph_*.h'
+	check_folder "c" src/ 'oph_*.c'
+	check_folder "c" test/ 'oph_*.c'
 
 	R=$(git status | grep "modified" | wc -l)
 	if [ $R -eq 0 ]
@@ -153,8 +162,24 @@ fi
 
 if [ "${package}" == "terminal" ] || [ $# -lt 3 ]; then
 
-	check_folder src/ 'oph_*.h'
-	check_folder src/ 'oph_*.c'
+	check_folder "c" src/ 'oph_*.h'
+	check_folder "c" src/ 'oph_*.c'
+
+	R=$(git status | grep "modified" | wc -l)
+	if [ $R -eq 0 ]
+		then 
+			echo "SUCCESS: all files are compliant with coding style"
+			$(exit 0) 
+		else 
+			echo "WARNING: found $R files not compliant:"
+			git status | grep "modified" | sed -n "s/^.*modified:[ ]*//p"
+			$(exit 1) 
+	fi
+fi
+
+if [ "${package}" == "PyOphidia" ] || [ $# -lt 3 ]; then
+
+	check_folder "python" PyOphidia/ '\[^_]*.py'
 
 	R=$(git status | grep "modified" | wc -l)
 	if [ $R -eq 0 ]
